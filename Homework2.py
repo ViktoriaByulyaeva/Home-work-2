9ee1982c344e6e29e44e481b9a67bf9c18fe9a61
Виктория <viktoriabyulyaeva@yandex.com>
2024-11-27 19:12:00 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,26 @@
+# 1st program
+
+print (9**0.5)
+print (5*(9**0.5))
+
+#2nd program
+print (9.99>9.98 and 1000!=1000.1)
+type (bool)
+
+#3rd program
+print (2*2+2)
+print ((2*2)+2)
+
+print ((2*2+2)==((2*2)+2))
+type (bool)
+
+#4th program
+print ('123.456')
+type (str)
+
+print ("123.456*10")
+type (str)
+
+print ((123.456*10)%10)
+type (int)
+
